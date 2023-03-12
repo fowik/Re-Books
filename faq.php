7a715e0b66df7829b51d9aecbaaee5fc4a2ff119
unknown <emilstrelis@inbox.lv>
2023-03-12 23:07:35 +0200

    instructions

--- faq.php
+++ faq.php
@@ -12,6 +12,7 @@ require_once "vendor/connect.php";
     <link rel="stylesheet" href="./assets/css/nav-bar.css">
     <link rel="stylesheet" href="./assets/css/footer.css">
     <link rel="stylesheet" href="./assets/css/faq.css">
+    <link href="http://fonts.cdnfonts.com/css/roboto" rel="stylesheet">
     <title>Lietotāja instrukcija</title>
 </head>
 <body>
@@ -21,36 +22,41 @@ require_once "vendor/connect.php";
 
         <div class="main">
             <div class="user_instruction">
-                <h1>Lietotājs</h1>
-                Dotajā mājaslapā lietotājs sākumā varēs tikai apskatīt, meklēt grāmatas pēc nosaukuma vai kategorijām, kā arī ir iespēja aizpildīt kontaktformu, lai sazinātos ar administrāciju. Lai parādītos iespēja lasīt, pievienot un vērtēt grāmatas, lietotājam vajadzēs piereģistrēties. Reģistrētajam lietotājam parādās daudz citas iespējas, piemēram, kā lasīt, pievienot, vērtēt grāmatas. Savā profilā lietotājs varēs meklēt sev pievienotās grāmatās vai arī dzēst tās no sava profila.
+                <h1 style="margin-left: 24px; margin-bottom:10px;">Lietotājs</h1>
+                <hr style="height:1px; background-color:white; margin-bottom:20px; margin-left:15px; margin-right:7px; margin-top:0px;">
+                <p style="margin-top: -10px; margin-left: 24px;">Dotajā mājaslapā lietotājs sākumā varēs tikai apskatīt, meklēt grāmatas pēc nosaukuma vai kategorijām, kā arī ir iespēja aizpildīt kontaktformu,
+                    lai sazinātos ar administrāciju. Lai parādītos iespēja lasīt, pievienot un vērtēt grāmatas, lietotājam vajadzēs piereģistrēties. Reģistrētajam 
+                    lietotājam parādās daudz citas iespējas, piemēram, kā lasīt, pievienot, vērtēt grāmatas. Savā profilā lietotājs varēs meklēt sev pievienotās
+                    grāmatās vai arī dzēst tās no sava profila.
+                </p>
                 <ul>
                     <li>
                     <h3>Meklēšanas josla</h3> <br>
-                        <span>
+                        <p>
                         Lietotājs var meklēt grāmatas pēc nosaukuma vai izvēloties atbilstošo kategoriju - parādīsies grāmatu grupa.
-                        </span>
+                        </p>
                     </li>
                     <li>
                     <h3>Reģistrācija</h3> <br>
-                        <span>
+                        <p>
                         Reģistrācija dod dažus priekšrocības pret lietotajiem kuri nereģistrējas, piemēram: <br>
                         1.	Grāmatu pievienošana izlasei. <br>
                         2.	Grāmatu vērtēšana. <br>
                         3.	Ir iespēja saglabāt grāmatas sev profilā un dzēst no tā. <br>
                         4.	Sazināšana ar administraciju(jāizpilda formu).
-                        </span>
+                        </p>
                     </li>
                     <li>
                     <h3>Grāmatas pievienošana</h3> <br>
-                        <span>
+                        <p>
                         Lietotājs var pievienot sev profilā grāmatas, kuri iepatikās viņam.
-                        </span>
+                        </p>
                     </li>
                     <li>
                     <h3>Papildinformācija</h3> <br>
-                        <span>
+                        <p>
                         Pašā apakšā ir pogas, kur var atrast blogu, cenas, informāciju un kontaktus.
-                        </span>
+                        </p>
                     </li>
                 </ul>
             </div>
@@ -60,18 +66,23 @@ require_once "vendor/connect.php";
                 if ($admin === '1') {
                     echo "
                     <div class='admin_instruction'>
-                    <h1>Administrators</h1>
-                    Administratoram ir pieejamas visas iespējas, kas ir dotas reģistrētajam lietotājam un pat vairāk. Administratoram vēl parādās iespēja pievienot, dzēst un rediģēt grāmatas, kā arī rediģēt, meklēt un dzēst lietotājus. Var rediģēt/pievienot tādas opcijas grāmatām: nosaukums, autors, apraksts, bilde, kategorija un datums. Lietotājam var rediģēt tādas opcijas kā e-pastu, lietotājvārdu un administratora tiesības. Dzēšot lietotāju vai grāmatu, tie arī izdzēsās pilnībā no datubāzes.
+                    <h1 style='margin-left: 24px; margin-bottom: 10px;'>Administrators</h1>
+                    <hr style='height:1px; background-color:white; margin-bottom:20px; margin-left:15px; margin-right:7px; margin-top:0px;'>
+                    <p style='margin-top: -10px; margin-left: 24px;'>Administratoram ir pieejamas visas iespējas, kas ir dotas reģistrētajam
+                    lietotājam un pat vairāk. Administratoram vēl parādās iespēja pievienot, dzēst un rediģēt grāmatas, kā arī rediģēt, meklēt un dzēst lietotājus. Var
+                    rediģēt/pievienot tādas opcijas grāmatām: nosaukums, autors, apraksts, bilde, kategorija un datums. Lietotājam var rediģēt tādas opcijas kā e-pastu,
+                    lietotājvārdu un administratora tiesības. Dzēšot lietotāju vai grāmatu, tie arī izdzēsās pilnībā no datubāzes.
+                    </p>
                     <ul>
                         <li>
-                        Profila meklēšanas josla <br>
-                            <span>
+                        <h3> Profila meklēšanas josla </h3> <br>
+                            <p>
                             Ir iespēja meklēt lietotājus un grāmatas profilā pēc nosaukuma. Ievadot vismaz vienu burtu tiek izvadīti visi rezultāti ar meklēto burtu nosaukumā, ja ievadīt divus burtus tiek izvadīti nosaukumi ar tiem diviem burtiem nosaukumā u.t.t.
-                            </span>
+                            </p>
                         </li>
                         <li>
-                        Grāmatas rediģēšana <br>
-                            <span>
+                        <h3> Grāmatas rediģēšana </h3> <br>
+                            <p>
                             Administrators var rediģēt grāmatu datus, ja ir parādījusies kaut kāda kļūda, jeb cits iemesls. Beidzot reģistrāciju grāmatas dati atjaunojās datubāzē. Rediģējot grāmatu, administratoram ir iespēja izmainīt: <br>
                             1.	Grāmatas nosaukumu <br>
                             2.	Grāmatas autoru <br>
@@ -79,26 +90,26 @@ require_once "vendor/connect.php";
                             4.	Bildi grāmatas vākam <br>
                             5.	Kategoriju kura pieder dotai grāmatai <br>
                             6.	Datumu kurā grāmata tika pievienota mājaslapai
-                            </span>
+                            </p>
                         </li>
                         <li>
-                        Lietotāja rediģēšana <br>
-                            <span>
+                        <h3> Lietotāja rediģēšana </h3> <br>
+                            <p>
                             Administrators var rediģēt lietotāja datus, ja lietotājs ir aizrakstījis vēstuli šajā sakarā, jeb lietotāja vārds vai e-pasts satur necenzētu leksiku, jeb, ja rodas iemesls iedot piekļuvi pie administratora tiesībām. Beidzot reģistrāciju lietotāja dati atjaunojās datubāzē. Rediģējot lietotāju, administratoram ir iespēja izmainīt: <br>
                             1.	Lietotāja profila vārdu <br>
                             2.	Lietotāja e-pastu <br>
                             3.	Piekļuvi pie administratora tiesībām
-                            </span>
+                            </p>
                         </li>
                         <li>
-                        Grāmatas/lietotāja dzēšana <br>
-                            <span>
+                        <h3> Grāmatas/lietotāja dzēšana </h3> <br>
+                            <p>
                             Dota iespēja izdzēst lietotāju vai arī grāmatu no mājaslapas datubāzes. Grāmatu var izdzēst, ja grāmatas autors nevēlas redzēt savu grāmatu mūsu mājaslapā. Lietotāju var izdzēst, ja viņš pārkāpj lietošanas noteikumus vairākas reizes.
-                            </span>
+                            </p>
                         </li>
                         <li>
-                        Grāmatas pievienošana <br>
-                            <span>
+                        <h3> Grāmatas pievienošana </h3> <br>
+                            <p>
                             Administrators var pēc izvēles pievienot jebkuru jaunu grāmatu mājaslapā. Pēc veiksmīgas pievienošanas, lietotājam uzreiz parādīsies iespēja to grāmatu sākt lasīt. Lai pievienotu grāmatu vajag pievienot šādus datus: <br>
                             1.	Grāmatas nosaukumu <br>
                             2.	Grāmatas autoru <br>
@@ -106,8 +117,7 @@ require_once "vendor/connect.php";
                             4.	Bildi grāmatas vākam <br>
                             5.	Kategoriju kura pieder dotai grāmatai <br>
                             6.	Datumu kurā grāmata tika pievienota mājaslapai
-    
-                            </span>
+                            </p>
                         </li>
                     </ul>
                 </div>
